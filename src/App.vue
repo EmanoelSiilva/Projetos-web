ef107ca38067c692dedecd05ed0c05664979183a
Emanoel <emanoelsilvaif@gmail.com>
2023-03-15 22:38:27 -0300

    adicionando meu projeto no github

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Formulário</h2>
+  <div class="mainDiv">
+    <div>
+      <label >Nome completo:</label>
+      <input type="text" class="fullName" v-model="fullName">
+      
+      <button @click="splitName" class="btn">>>></button>
+    </div>
+
+    <div class="divSplitName">
+      <p>Nome</p>
+      <input type="text" class="inputSplit" :value="firstName">
+      <p>Sobrenome</p>
+      <input type="text" class="inputSplit" :value="lastName">
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      fullName: '',
+      firstName: [],
+      lastName: []
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  methods: {
+    splitName() {
+      this.firstName = this.fullName.split(" ")[0]
+      this.lastName = this.fullName.split(" ")[1]
+      console.log(this.firstName + ' ' + this.lastName) 
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
