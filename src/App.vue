50d36a8a67dc0727258ca1c751ab2f8e7b371828
Emanoel <emanoelsilvaif@gmail.com>
2023-03-15 22:40:22 -0300

    adicionando meu projeto no github

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Cálculo IMC</h1>
+  <div class="mainDiv">
+    <div class="getInfo">
+      <label for="">Nome</label>
+      <input type="text" v-model="name">
+      <label for="">Peso</label>
+      <input type="text" v-model="peso">
+      <label for="">Altura</label>
+      <input type="text" v-model="altura">
+      <button @click="calculaIMC()">Calcular IMC</button>
+    </div>
+
+    <div class="resultDiv">
+      <span>
+        <p v-if="this.imc != ''">Olá {{ name }}! O seu IMC é de {{ imc }}</p>
+      </span>
+    </div>
+    
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: '',
+      altura: 0,
+      peso: 0,
+      imc: ''
+    }
+  },
+  methods: {
+    calculaIMC() {
+      const imc = this.peso / Math.pow(this.altura, 2)
+      this.imc = imc.toFixed(2)
+      console.log(this.imc)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  h1 {
+    text-align: center;
+  }
+  
+  .getInfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .getInfo input {
+    width: 250px;
+  }
+
+  .getInfo button {
+    border-radius: 25px;
+    margin: 20px;
+  }
+  .mainDiv {
+    border: 1px solid #9359c988;
+    padding: 5px;
+    margin: 10px;
+    padding: 10px;
+  }
+
+  .resultDiv {
+    border: 1px solid #9359c988;
+    padding: 5px;
+    margin: 10px;
+    height: 70px;
+  }
 </style>
